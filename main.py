5190d0f6dfa64b646eda2fcec139c4fd1d91cc3f
Madison <125610265+MadisonW5@users.noreply.github.com>
2026-02-24 16:51:34 -0500

    date turned back to normal

--- main.py
+++ main.py
@@ -245,10 +245,9 @@ def fill_out_US_shipping_invoices(uploaded_excel):
                 widgets[129].field_value = str(round(sum(amount),2))
                 widgets[129].update() #total amount field
 
-                widgets[131].field_value = "02/17/2026"#datetime.now().strftime("%m/%d/%Y")
+                widgets[131].field_value = datetime.now().strftime("%m/%d/%Y")
                 widgets[131].update() #date field
                 
-                #widget_counter += 1
 
         pdf_bytes = doc.write()
         invoices.append((output_file_name, pdf_bytes))
