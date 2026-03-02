b8c770e9932ee4f37fe36a9dec35fee59708e951
Madison <125610265+MadisonW5@users.noreply.github.com>
2026-03-02 15:02:52 -0500

    put an in statement to hide next part of code if the try catch found an error

--- website.py
+++ website.py
@@ -14,15 +14,16 @@ if uploaded_excel and st.button("Fill out Invoices"):
     with zipfile.ZipFile(zip_buffer,"w") as zip_file:
         invoices = main.fill_out_US_shipping_invoices(uploaded_excel)
 
-        if invoices == True:
+        if type(invoices) is bool:
             st.header("You forgot to delete an empty page in the Excel sheet. Edit the sheet and try again.")
 
         else:
             for filename, pdf_bytes in invoices:
                 zip_file.writestr(filename, pdf_bytes)
 
-    zip_buffer.seek(0)
+    if type(invoices) is bool:
+        zip_buffer.seek(0)
 
-    st.header("Invoices Complete")
+        st.header("Invoices Complete")
 
-    st.download_button("Download Filled Invoices", data=zip_buffer, file_name="US Customs Invoices " + datetime.now().strftime("%m-%d-%Y") + ".zip", mime="application/zip")
+        st.download_button("Download Filled Invoices", data=zip_buffer, file_name="US Customs Invoices " + datetime.now().strftime("%m-%d-%Y") + ".zip", mime="application/zip")
